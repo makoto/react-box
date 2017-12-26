0305c9d28b395e0db241695f531c8d8c12373418
Makoto Inoue <inouemak@googlemail.com>
2017-12-26 22:01:21 +0700

    Add component spec

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,8 +1,27 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import App from './App'
+import { mount, configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-15.4';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+configure({ adapter: new Adapter() });
+
+function setup() {
+  const props = {}
+
+  const enzymeWrapper = mount(<App {...props} />)
+
+  return {
+    props,
+    enzymeWrapper
+  }
+}
+
+describe('components', () => {
+  describe('App', () => {
+    it('should render self', () => {
+      const { enzymeWrapper } = setup()
+      expect(enzymeWrapper.find('.App').hasClass('App')).toBe(true)
+      expect(enzymeWrapper.find('h1').text()).toBe('Good to Go!')
+    })
+  })
 })
